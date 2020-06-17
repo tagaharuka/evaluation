c58057633a8301b3224b9ba845a3ae0b89708554
tagaharuka <38071941+tagaharuka@users.noreply.github.com>
2020-06-18 02:01:17 +0900

    Update mos.js

--- mos.js
+++ mos.js
@@ -134,12 +134,12 @@ function setAudio()
 
 function resetForm()
 {
-//     document.getElementById("check1").checked = false;
-//     document.getElementById("check2").checked = false;
-//     document.getElementById("check3").checked = false;
-//     document.getElementById("check4").checked = false;
-//     document.getElementById("check5").checked = false;
-    document.getElementByName("eval").checked = false;
+    document.getElementById("check1").checked = false;
+    document.getElementById("check2").checked = false;
+    document.getElementById("check3").checked = false;
+    document.getElementById("check4").checked = false;
+    document.getElementById("check5").checked = false;
+//     document.getElementByName("eval").checked = false;
 //     return false;
 }
 
